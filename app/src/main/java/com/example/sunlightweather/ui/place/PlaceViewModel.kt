f3d442c9da2efca64d7ba3e65876cddf4fac37fd
sunlight-pixel <18597066502@163.comm>
2020-12-25 11:11:49 +0800

    the citys' data

---
+++ app/src/main/java/com/example/sunlightweather/ui/place/PlaceViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.sunlightweather.ui.place
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.example.sunlightweather.logic.Repository
+import com.example.sunlightweather.logic.model.Place
+import java.util.ArrayList
+
+class PlaceViewModel : ViewModel() {
+
+    private val searchLiveData = MutableLiveData<String>()
+
+    val placeList = ArrayList<Place>() //对界面上显示的城市数据进行缓存
+
+    val placeLiveData = Transformations.switchMap(searchLiveData) { query ->
+        Repository.searchPlaces(query)
+    }
+
+    fun searchPlaces(query: String) {
+        searchLiveData.value = query
+    }
+}
